1709ed86cf82423a51d02f34a1241e0b4fbff028
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-10-04 14:24:27 -0500

    Create day2_2.cs

---
+++ day2_2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+        var name = "Tom";
+		Console.WriteLine("My name is {0}", name);
+    }
+  }
+}
