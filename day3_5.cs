024326f8f04160806ce4fd9a1947d22d9c870c8f
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-10-05 21:53:16 -0500

    Create day3_5.cs

---
+++ day3_5.cs
@@ -0,0 +1,18 @@
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("The 9X9 chart");
+            for (int i = 1; i <= 9; i++)
+        {
+            for (int j=1; j <=9; j++)
+            {
+                Console.Write("{0}x{1}={2} ", i, j, i*j);
+            }
+            Console.WriteLine();
+        }
+    }   
+}
